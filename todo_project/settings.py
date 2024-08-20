2fff040e14041ea25c3f97c6c8557ffe40f76281
Yash Arya <yasharya2901@gmail.com>
2024-08-20 15:22:49 +0530

    Checkpoint 2: Implemented CRUD Functionality Created API Endpoints: Implement CRUD operations for the Todo model:   1. Create a new to-do.   2. Fetch all to-dos.   3. Edit an existing to-do.   4. Delete a to-do. Used djangorestframework library - Integrated DRF for API functionality.

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'todos'
+    'todos',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
+}
+
+APPEND_SLASH=False
--- todos/views.py
+++ todos/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import viewsets
+from .models import Todo
+from .serializers import TodoSerializer
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request):
+    return HttpResponse('Welcome to the Todo API!')
+
+class TodoViewSet(viewsets.ModelViewSet):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
